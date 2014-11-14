1d56b49c4d05176c773278edb63831ff47963fa7
Shivan <shivankaul.1993@gmail.com>
2014-11-14 05:11:43 -0500

    started working...

---
+++ ExperimentalAverage.c
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : ExperimentalAverage.c
+ Author      : Shivan Kaul Sahib
+ Version     : 1.0
+ Description : Q1 on assignment 3
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
